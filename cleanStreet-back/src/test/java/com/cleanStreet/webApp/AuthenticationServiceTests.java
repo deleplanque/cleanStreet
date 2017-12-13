739e0fcae41f1541304900cb857694048a2bf41d
Lequette Teddy <lequette@b13p20.fil.univ-lille1.fr>
2017-12-13 19:26:34 +0100

    Test Méthodes de validation

--- cleanStreet-back/src/test/java/com/cleanStreet/webApp/AuthenticationServiceTests.java
+++ cleanStreet-back/src/test/java/com/cleanStreet/webApp/AuthenticationServiceTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.security.Provider.Service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,12 @@ public class AuthenticationServiceTests {
 		@Autowired
 		AuthentificationService authService;
 		
+		@Before
+		public void init(){
+			
+			
+		}
+		
 		@Test
 		public void connectionTest(){
 			ConnexionForm cf = new ConnexionForm();
@@ -36,5 +43,41 @@ public class AuthenticationServiceTests {
 			assertEquals(u.toString(),authService.connexion(cf).toString());
 			
 			
+		}
+		
+		@Test
+		public void inscriptionTest(){
+			Utilisateur cf = new Utilisateur();
+			cf.setEmail("d@d.net");
+			cf.setMotDePasse("azerty");
+			cf.setId(65);
+			cf.setNom("rrefr");
+			cf.setPrenom("trgrtef");
+			authService.inscription(cf);
+			assertTrue(authService.connexion(cf.getEmail(), cf.getMotDePasse()));
+
+		}
+		
+		@Test
+		public void controlesTests(){
+			Utilisateur cf = new Utilisateur();
+			cf.setEmail("d@d.net");
+			cf.setMotDePasse("azerty");
+			cf.setId(65);
+			cf.setNom("rrefr");
+			cf.setPrenom("trgrtef");
+			
+			Utilisateur cf1 = new Utilisateur();
+			cf1.setEmail("d@net");
+			cf1.setMotDePasse("aerty");
+			cf1.setId(65);
+			cf1.setNom("rre5fr");
+			cf1.setPrenom("trgr5tef");
+			
+			assertTrue(authService.coordonneesValides(cf));
+			assertFalse(authService.coordonneesValides(cf1));
+			assertFalse(authService.controleMail(cf1.getEmail()));
+			assertFalse(authService.controlePrenom(cf1.getPrenom()));
+			assertFalse(authService.controleMdp(cf1.getMotDePasse()));
 		}
 }
