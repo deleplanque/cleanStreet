fee5e220f18a139090e7148289e85f92a5ff3fae
deleplanque <d.dylan62138@gmail.com>
2018-03-14 10:25:16 +0100

    modification du style ainsi que mise en place de la fonction permettant de calculer la distance entre deux points

--- cleanStreet-back/src/main/java/com/cleanStreet/webApp/services/SignalementService.java
+++ cleanStreet-back/src/main/java/com/cleanStreet/webApp/services/SignalementService.java
@@ -12,11 +12,11 @@ import com.cleanStreet.webApp.entite.Quartier;
 import com.cleanStreet.webApp.entite.Signalement;
 
 @Service
-public class SignalementService implements ISignalementService{
+public class SignalementService implements ISignalementService {
 
 	@Autowired
 	ISignalementDAO signalementDAO;
-	
+
 	@Autowired
 	ILocalisationService localisationService;
 
@@ -26,23 +26,21 @@ public class SignalementService implements ISignalementService{
 	@Override
 	public Signalement ajouterSignalement(Signalement signalement) {
 		// You can change the Image URL accordingly.
-				String imageUrl = "C:/Users/david/Pictures/pistolet.jpg";
-			
-				// List of Recognized Result from Image
-				List<String> resultList = ClarifaiApi.recognize(imageUrl);
-				if(resultList.contains("marijuana") || resultList.contains("human") || resultList.contains("nude") 
-						|| resultList.contains("weapon") || resultList.contains("gore") || resultList.contains("drug")){
-					System.out.println(true);
-				}else{
-					System.out.println(false);
-					return signalementDAO.saveAndFlush(signalement);
-				}
-				// Iteration of Result
-				for(String result : resultList) {
-					System.out.println(result);
-				}	
-				return null;
-			
+		String imageUrl = "C:/Users/david/Pictures/pistolet.jpg";
+
+		List<String> resultList = ClarifaiApi.recognize(imageUrl);
+		if (resultList.contains("marijuana") || resultList.contains("human") || resultList.contains("nude")
+				|| resultList.contains("weapon") || resultList.contains("gore") || resultList.contains("drug")) {
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+			return signalementDAO.saveAndFlush(signalement);
+		}
+		for (String result : resultList) {
+			System.out.println(result);
+		}
+		return null;
+
 	}
 
 	@Override
@@ -76,4 +74,21 @@ public class SignalementService implements ISignalementService{
 		return quartierDAO.findAll();
 	}
 
+	public double deg2rad(double x) {
+		return Math.PI * x / 180;
+	}
+
+	public double get_distance_m(double lat1, double lng1, double lat2, double lng2) {
+		int earth_radius = 6378137;
+		double rlo1 = deg2rad(lng1); 
+		double rla1 = deg2rad(lat1);
+		double rlo2 = deg2rad(lng2);
+		double rla2 = deg2rad(lat2);
+		double dlo = (rlo2 - rlo1) / 2;
+		double dla = (rla2 - rla1) / 2;
+		double a = (Math.sin(dla) * Math.sin(dla))
+				+ Math.cos(rla1) * Math.cos(rla2) * (Math.sin(dlo) * Math.sin(dlo));
+		double d = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		return (earth_radius * d);
+	}
 }
